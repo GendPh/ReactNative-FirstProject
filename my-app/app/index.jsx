d31f1f3e58cb04df75fc1bc69c5772d51f66bab1
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-10-07 15:25:31 +0100

    Update tsconfig.json and babel.config.js

--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -4,8 +4,8 @@ import { StatusBar } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl">Gabriel</Text>
       <StatusBar style="auto" />
 
       <Link href="/profile">Go to Profile</Link>
